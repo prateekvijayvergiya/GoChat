c2848c068f0d7b6b8826d36048abff43aa195d37
prateekvijayvergiya <prateek.vijayudr27@gmail.com>
2018-01-13 01:42:31 +0530

    Firebase added

--- app/src/main/java/com/bunny/gochat/RegisterActivity.java
+++ app/src/main/java/com/bunny/gochat/RegisterActivity.java
@@ -16,6 +16,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -26,6 +31,7 @@ public class RegisterActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private Toolbar mToolbar;
     private ProgressDialog mRegProgress;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,19 +70,35 @@ public class RegisterActivity extends AppCompatActivity {
 
     }
 
-    private void registerUser(String name, String email, String password) {
+    private void registerUser(final String name, String email, String password) {
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
 
-                            Toast.makeText(RegisterActivity.this, "Authentication Done.", Toast.LENGTH_SHORT).show();
-                            mRegProgress.dismiss();
-                            Intent mainIntent = new Intent(RegisterActivity.this,MainActivity.class);
-                            mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                            startActivity(mainIntent);
-                            finish();
+                            FirebaseUser current_user = FirebaseAuth.getInstance().getCurrentUser();
+                            String uid = current_user.getUid();
+
+                            databaseReference = FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
+
+                            HashMap<String,String> userMap = new HashMap<>();
+                            userMap.put("name",name);
+                            userMap.put("status","Hi there I am using GoChat");
+                            userMap.put("image","default");
+                            userMap.put("thumb_image","default");
+                            databaseReference.setValue(userMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    Toast.makeText(RegisterActivity.this, "Authentication Done.", Toast.LENGTH_SHORT).show();
+                                    mRegProgress.dismiss();
+                                    Intent mainIntent = new Intent(RegisterActivity.this,MainActivity.class);
+                                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                    startActivity(mainIntent);
+                                    finish();
+                                }
+                            });
+
                             // Sign in success, update UI with the signed-in user's information
                             //Log.d(TAG, "createUserWithEmail:success");
                             // FirebaseUser user = mAuth.getCurrentUser();
