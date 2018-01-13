d134d3045e21058175ace4a02848050d87f47dae
prateekvijayvergiya <prateek.vijayudr27@gmail.com>
2018-01-13 21:17:22 +0530

    some changes

--- app/src/main/java/com/bunny/gochat/SettingsActivity.java
+++ app/src/main/java/com/bunny/gochat/SettingsActivity.java
@@ -1,12 +1,17 @@
 package com.bunny.gochat;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -14,6 +19,13 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.util.Random;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -27,6 +39,9 @@ public class SettingsActivity extends AppCompatActivity {
     private TextView mStatus;
     private Button mSettingsStatusBtn;
     private Button msettingsImageBtn;
+    private static final int GALLERY_PICK=1;
+    private StorageReference storageReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +52,8 @@ public class SettingsActivity extends AppCompatActivity {
         mStatus = (TextView) findViewById(R.id.statusName);
         mSettingsStatusBtn = (Button) findViewById(R.id.settingsStatusBtn);
         msettingsImageBtn = (Button) findViewById(R.id.settingsImageBtn);
+
+        storageReference = FirebaseStorage.getInstance().getReference();
 
         currentUser = FirebaseAuth.getInstance().getCurrentUser();
         String uid = currentUser.getUid();
@@ -70,5 +87,63 @@ public class SettingsActivity extends AppCompatActivity {
            }
        });
 
+       msettingsImageBtn.setOnClickListener(new View.OnClickListener() {
+          @Override
+           public void onClick(View view) {
+               /*Intent galleryIntent = new Intent();
+               galleryIntent.setType("Image/*");
+               galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+               startActivityForResult(Intent.createChooser(galleryIntent,"Select Image"),GALLERY_PICK);*/
+
+               CropImage.activity()
+                       .setGuidelines(CropImageView.Guidelines.ON)
+                       .start(SettingsActivity.this);
+
+           }
+       });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                StorageReference filePath = storageReference.child("profile_iamges").child(random()+".jpg");
+                filePath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(SettingsActivity.this,"Loading",Toast.LENGTH_LONG).show();
+                        }else{
+                            Toast.makeText(SettingsActivity.this," Not Loading",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
+
+        /*if (requestCode==GALLERY_PICK && resultCode == RESULT_OK){
+            Uri imageUri = data.getData();
+            CropImage.activity(imageUri).setAspectRatio(1,1)
+                    .start(this);
+        }*/
+    }
+
+    public static String random() {
+        Random generator = new Random();
+        StringBuilder randomStringBuilder = new StringBuilder();
+        int randomLength = generator.nextInt(10);
+        char tempChar;
+        for (int i = 0; i < randomLength; i++){
+            tempChar = (char) (generator.nextInt(96) + 32);
+            randomStringBuilder.append(tempChar);
+        }
+        return randomStringBuilder.toString();
     }
 }
