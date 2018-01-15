555e7452667828c63e94d8bcf2673c5054403c2f
prateekvijayvergiya <prateek.vijayudr27@gmail.com>
2018-01-15 22:26:53 +0530

    All users Completed

--- app/src/main/java/com/bunny/gochat/ProfileActivity.java
+++ app/src/main/java/com/bunny/gochat/ProfileActivity.java
@@ -2,14 +2,19 @@ package com.bunny.gochat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private TextView displayName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-
+        String userId = getIntent().getStringExtra("userId");
+        displayName = (TextView) findViewById(R.id.displayName);
+        displayName.setText(userId);
     }
 }
