9979783f50da7a2bd055a118ab79961b877f0a3d
prateekvijayvergiya <prateek.vijayudr27@gmail.com>
2018-01-25 20:18:10 +0530

    Notification updated

---
+++ app/src/main/java/com/bunny/gochat/FirebaseMessagingService.java
@@ -0,0 +1,44 @@
+package com.bunny.gochat;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by hp on 25-01-2018.
+ */
+
+public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        String notificationTitle = remoteMessage.getNotification().getTitle();
+        String notificationMessage = remoteMessage.getNotification().getBody();
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.profile_icon)
+                        .setContentTitle(notificationTitle)
+                        .setContentText(notificationMessage);
+
+        Intent resultIntent = new Intent();
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(
+                        this,
+                        0,
+                        resultIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+
+
+        int mNotificationId = (int) System.currentTimeMillis();
+        NotificationManager mNotifyMgr =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mNotifyMgr.notify(mNotificationId, mBuilder.build());
+    }
+}
