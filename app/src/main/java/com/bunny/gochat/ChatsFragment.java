fdbb4e3a78767a955e8d08545e99f1ca79fa44ef
prateekvijayvergiya <prateek.vijayudr27@gmail.com>
2018-02-04 21:42:45 +0530

    minor fixes

--- app/src/main/java/com/bunny/gochat/ChatsFragment.java
+++ app/src/main/java/com/bunny/gochat/ChatsFragment.java
@@ -24,9 +24,6 @@ import com.squareup.picasso.Picasso;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class ChatsFragment extends Fragment {
 
     private View mMainView;
@@ -37,14 +34,14 @@ public class ChatsFragment extends Fragment {
     private DatabaseReference mChatDatabase;
 
     public ChatsFragment() {
-        // Required empty public constructor
+
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         mMainView = inflater.inflate(R.layout.fragment_chats, container, false);
         mChatList = (RecyclerView) mMainView.findViewById(R.id.chatList);
         mAuth = FirebaseAuth.getInstance();
